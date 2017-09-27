4a9c46a8da27c38e1be659631fc6949f8ae3e60a
brsull01 <brsull01@cardmail.louisville.edu>
2017-09-27 13:45:19 -0400

    Intial commit.

---
+++ content.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+  // <h1>Welcome!</h1>
+  // <p> My name is Bryce Im not nice and I eat rice</p>
+  // <p>Hello.<p>
+
+
+// parent.createElement("child");
+
+let h1=document.createElement("h1");
+      h1.innerHTML="Welcome";
+
+      document.appendChild(h1)
+
+}
